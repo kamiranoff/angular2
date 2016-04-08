cc6f75c5144d1f75bf704de4fa72f0bb8b39456c
Kevin Amiranoff <kamiranoff@gmail.com>
2016-04-08 18:56:40 +0200

    adds routing capatibilities and better organize app

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,5 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+//Currently watching https://angular.io/docs/ts/latest/tutorial/toh-pt5.html
+System.register(['angular2/core', 'angular2/router', './heroes.component', './hero.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,21 +11,42 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1, heroes_component_1, hero_service_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (heroes_component_1_1) {
+                heroes_component_1 = heroes_component_1_1;
+            },
+            function (hero_service_1_1) {
+                hero_service_1 = hero_service_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                 }
                 AppComponent = __decorate([
+                    router_1.RouteConfig([
+                        {
+                            path: '/heroes',
+                            name: 'Heroes',
+                            component: heroes_component_1.HeroesComponent
+                        }
+                    ]),
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: "\n  <h1>{{title}}</h1>\n  <a [routerLink]=\"['Heroes']\">Heroes</a>\n  <router-outlet></router-outlet>\n  ",
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [
+                            router_1.ROUTER_PROVIDERS,
+                            hero_service_1.HeroService
+                        ]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,35 @@
+//Currently watching https://angular.io/docs/ts/latest/tutorial/toh-pt5.html
+
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
+
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template:`
+  <h1>{{title}}</h1>
+  <a [routerLink]="['Heroes']">Heroes</a>
+  <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES],//allow us to use the directive <my-heroes> and routing functionnalities in template
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title: 'HEROES OF ANGULAR 2';
+
+}
